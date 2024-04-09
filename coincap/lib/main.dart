16b283b066dda04a777f840ed673e65bb703ad70
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-09 12:02:53 +0545

    modified

--- coincap/lib/main.dart
+++ coincap/lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:convert';
+
 import 'package:coincap/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String configContent = await rootBundle.loadString("assets/config/main.json");
+  Map confgData = jsonDecode(configContent);
+  print(confgData);
 }
 
 class MyApp extends StatelessWidget {
@@ -11,10 +22,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromRGBO(88, 60, 197, 1.0),
       ),
       home: const HomePage(),
     );
