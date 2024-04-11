824e0d3fbbe9d047defb4cc8719bd7532f9da62f
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-11 23:51:57 +0545

    modified further

--- finstagram/lib/pages/home_page.dart
+++ finstagram/lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:finstagram/pages/feed_page.dart';
+import 'package:finstagram/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +10,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currPageIndex = 0;
+  final List<Widget> _pages = const [
+    FeedPage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          "Finstagram",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: const Icon(
+              Icons.add_a_photo,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: _buttomNavigation(),
+      body: _pages[currPageIndex],
+      backgroundColor: Colors.green,
+    );
+  }
+
+  Widget _buttomNavigation() {
+    return BottomNavigationBar(
+      onTap: (value) {
+        setState(() {
+          currPageIndex = value;
+          print(currPageIndex);
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: "Home"),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+          ),
+          label: "Profile",
+        ),
+      ],
+    );
   }
 }
