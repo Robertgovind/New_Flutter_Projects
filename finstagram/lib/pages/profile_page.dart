7ddc9a44a3dc1fb9b5189a1559352df76000a97a
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-11 23:50:44 +0545

    working on  profile page

---
+++ finstagram/lib/pages/profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  double? _deviceHeight, _deviceWidth;
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Container(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      color: Colors.amber,
+    );
+  }
+}
