9bd2d87b038ad5a30e860247b74558e6e3d1cf3c
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-15 09:34:09 +0545

    implemented delete info

--- employee/lib/services/database.dart
+++ employee/lib/services/database.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//CRUD OPERATION
 class DatabaseMethod {
+  //Create
   Future addEmployeeDetails(
       Map<String, dynamic> employeeInfo, String id) async {
     return await FirebaseFirestore.instance
@@ -9,7 +11,24 @@ class DatabaseMethod {
         .set(employeeInfo);
   }
 
+  //Read
   Future<Stream<QuerySnapshot>> getEmployee() async {
     return await FirebaseFirestore.instance.collection('Employee').snapshots();
+  }
+
+  //Update
+  Future updateEmpDetails(Map<String, dynamic> info, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .update(info);
+  }
+
+  //Delete
+  Future deleteEmpDetails(String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .delete();
   }
 }
