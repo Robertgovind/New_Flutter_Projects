45739245dba279c32d2bfa36c0bdfe4897aaf3a1
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-15 08:52:04 +0545

    database integration

--- employee/lib/services/database.dart
+++ employee/lib/services/database.dart
@@ -3,6 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseMethod {
   Future addEmployeeDetails(
       Map<String, dynamic> employeeInfo, String id) async {
-    return await FirebaseFirestore.instance.collection('Employee').doc(id);
+    return await FirebaseFirestore.instance
+        .collection('Employee')
+        .doc(id)
+        .set(employeeInfo);
+  }
+
+  Future<Stream<QuerySnapshot>> getEmployee() async {
+    return await FirebaseFirestore.instance.collection('Employee').snapshots();
   }
 }
